fd085b8de51336d68fb7b8021078e10c8d98d2f7
caamir <AMIR@Amirs-MacBook-Pro.local>
2018-02-12 13:52:09 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.header['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", function (req, res) {
+  res.render("index.html");
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Roll-Dice Server Is On Port 3000");
+});
